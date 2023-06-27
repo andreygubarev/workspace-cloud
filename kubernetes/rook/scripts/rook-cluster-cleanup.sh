c9b1999a0cc3f29c6ebeed889c7b80db66eb4a7e
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-27 11:55:33 +0400

    add cluster cleanup

---
+++ kubernetes/rook/scripts/rook-cluster-cleanup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
+    kubectl get -n rook-ceph "$CRD" -o name | \
+    xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
+done
