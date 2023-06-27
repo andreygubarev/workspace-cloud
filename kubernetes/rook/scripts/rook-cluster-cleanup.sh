71ba19060b5ab8832fb97f67c22fdb59ae3efd69
Andrey Gubarev <andrey@andreygubarev.com>
2023-06-27 14:03:59 +0400

    update make

--- kubernetes/rook/scripts/rook-cluster-cleanup.sh
+++ kubernetes/rook/scripts/rook-cluster-cleanup.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-for CRD in $(kubectl get crd -n rook-ceph | awk '/ceph.rook.io/ {print $1}'); do
-    kubectl get -n rook-ceph "$CRD" -o name | \
-    xargs -I {} kubectl patch -n rook-ceph {} --type merge -p '{"metadata":{"finalizers": []}}'
+NS=rook-ceph-cluster
+
+for CRD in $(kubectl -n $NS get crd | awk '/ceph.rook.io/ {print $1}'); do
+    kubectl -n $NS get "$CRD" -o name | \
+    xargs -I {} kubectl -n $NS patch {} --type merge -p '{"metadata":{"finalizers": []}}'
 done
+
+if kubectl -n $NS get secret rook-ceph-mon; then
+    kubectl -n $NS patch secrets rook-ceph-mon --type merge -p '{"metadata":{"finalizers": []}}'
+fi
+
+if kubectl -n $NS get configmap rook-ceph-mon-endpoints; then
+    kubectl -n $NS patch configmap rook-ceph-mon-endpoints --type merge -p '{"metadata":{"finalizers": []}}'
+fi
+
+for NODE in $(kubectl get nodes -l 'topology.kubernetes.io/zone=rus-central-1' -o name); do
+    NODE=$(basename "$NODE")
+    ssh "$NODE" "sudo rm -rf /var/lib/rook/rook-ceph"
+done
+
+kubectl delete ns "$NS"
